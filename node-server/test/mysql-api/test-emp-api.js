a6829b997944ec8512860db757ab240b8898f14d
Pavlo Shcherbukha <pavlo.shcherbukha@pravex.ua>
2021-10-05 09:30:28 +0300

    clean code

--- node-server/test/mysql-api/test-emp-api.js
+++ node-server/test/mysql-api/test-emp-api.js
@@ -1,19 +1,3 @@
-/**
- * Тестовий модуля обработки файлов HRProcessor.js
- * 
- * 
- */
-
-
-/*
-const log4js = require('log4js');
-log4js.configure({
-    appenders: { cheese: { type: "file", filename: "cheese.log" } },
-    categories: { default: { appenders: ["cheese"], level: "error" } }
-  });
-const logger = log4js.getLogger("cheese");
-logger.level = process.env.LOG_LEVEL || 'info'
-*/
 
 const mocha = require('mocha');
 const chai = require('chai');
