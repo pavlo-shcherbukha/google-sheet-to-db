802adefa17b3a76361d1aee7bfc588d3bac51b29
pavlo-shcherbukha <pasha.kx@gmail.com>
2021-10-31 14:17:49 +0200

    Добавил еще один header

--- node-server/routes/emps.js
+++ node-server/routes/emps.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* OPTIONS users listing. */
 router.options('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, ad-name, x-powered-by, date");
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-powered-by, date");
   res.header('Access-Control-Allow-Methods', 'DELETE,GET,PATCH,POST,PUT'); 
   return res.status(200).end();
 
