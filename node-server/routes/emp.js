6f8fb1572a91ebf4d521039b94866acfd39bd400
pavlo-shcherbukha <pasha.kx@gmail.com>
2021-11-03 14:47:18 +0200

    options

--- node-server/routes/emp.js
+++ node-server/routes/emp.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
-
-
 router.options('/:id', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-powered-by, date");
